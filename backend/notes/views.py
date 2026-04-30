4bdd83f65d4b5c5623f76b2561e9bab3f7b5e403
PiotrPliszka <181106@student.uwm.edu.pl>
2026-04-30 18:45:51 +0200

    Adding basics endpoints

--- backend/notes/views.py
+++ backend/notes/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Note
+from .serializers import NoteSerializer
+
 
 # Create your views here.
+class NoteListCreateView(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(user=user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class NoteDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = NoteSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(user=user)
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .serializers import RegisterSerializer, UserSerializer
 
 # Create your views here.
+
+
+class UserCreateView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+
+class UserMeView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
