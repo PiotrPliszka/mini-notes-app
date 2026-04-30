2e3c79d2c53e71bbe6f36607f8d2e5e0ce46419a
PiotrPliszka <181106@student.uwm.edu.pl>
2026-04-30 14:15:16 +0200

    Adding User and Note model

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=64)
+    content = models.TextField(blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
