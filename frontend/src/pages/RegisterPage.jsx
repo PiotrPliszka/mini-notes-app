a4b7d5d6f33de1e662cccaa215c301077af5d339
PiotrPliszka <181106@student.uwm.edu.pl>
2026-05-04 10:05:07 +0200

    RegisterPage styles and layout

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,9 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./RegisterPage.css";
 
 export function RegisterPage() {
   return (
-    <div>
-      <h1>RegisterPage</h1>
+    <div className="main-container">
+      <div className="title-div">
+        <p className="mini-title">Auth</p>
+        <h1 className="about-app">Regitser or log in without friction</h1>
+      </div>
+      <div className="buttons-div">
+        <Link to="/register" className="register-btn">
+          Register
+        </Link>
+        <Link to="/login" className="login-btn">
+          Login
+        </Link>
+      </div>
+      <div className="form-div">
+        <form>
+          <label>Username</label>
+          <input type="text" />
+          <label>Email</label>
+          <input type="text" />
+          <label>Password</label>
+          <input type="text" />
+          <label>Confrim Password</label>
+          <input type="text" />
+          <button type="submit">Start Writing</button>
+        </form>
+      </div>
+      <div className="note-div">
+        <p className="note-title">Goode next step</p>
+        <span className="short-note">
+          After submit, enojy creating notes B)
+        </span>
+      </div>
     </div>
   );
 }
