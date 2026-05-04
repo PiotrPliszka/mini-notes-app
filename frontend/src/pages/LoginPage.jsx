0eadbe61863ab06c6a2c9b0e66df510e0719b083
PiotrPliszka <181106@student.uwm.edu.pl>
2026-05-04 10:14:51 +0200

    LoginPage styles and layout

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,9 +1,47 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "./LoginPage.css";
 
 export function LoginPage() {
   return (
-    <div>
-      <h1>LoginPage</h1>
+    <div className="main-container">
+      <div className="title-div">
+        <p className="mini-title">Auth</p>
+        <h1 className="about-app">Regitser or log in without friction</h1>
+      </div>
+      <div className="buttons-div">
+        <NavLink
+          to="/register"
+          className={({ isActive }) =>
+            `register-btn ${isActive ? "auth-btn-active" : "auth-btn-inactive"}`
+          }
+        >
+          Register
+        </NavLink>
+        <NavLink
+          to="/login"
+          className={({ isActive }) =>
+            `login-btn ${isActive ? "auth-btn-active" : "auth-btn-inactive"}`
+          }
+        >
+          Login
+        </NavLink>
+      </div>
+      <div className="form-div">
+        <form>
+          <label>Username</label>
+          <input type="text" />
+          <label>Password</label>
+          <input type="text" />
+          <button type="submit">Start Writing</button>
+        </form>
+      </div>
+      <div className="note-div">
+        <p className="note-title">Goode next step</p>
+        <span className="short-note">
+          After submit, enojy creating notes B)
+        </span>
+      </div>
     </div>
   );
 }
