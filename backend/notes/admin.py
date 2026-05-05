d39dab191934ea441394428881bd7ec8803234e1
PiotrPliszka <181106@student.uwm.edu.pl>
2026-05-05 16:52:06 +0200

    register models on adminsite

--- backend/notes/admin.py
+++ backend/notes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Note
 
 # Register your models here.
+
+
+class NoteAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Note, NoteAdmin)
