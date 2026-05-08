5a7e00288938d65eed751de08888a76bc425dfa8
PiotrPliszka <181106@student.uwm.edu.pl>
2026-05-08 19:02:37 +0200

    feat: add form validation handling and loading state

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -4,29 +4,52 @@ import icon from "../assets/house.svg";
 import api from "../api/axios";
 import "./LoginPage.css";
 import { useAuth } from "../context/AuthContext";
+import toast from "react-hot-toast";
+import { motion } from "framer-motion";
+import { Loader2 } from "lucide-react";
 
 export function LoginPage() {
   const navigate = useNavigate();
   const [user, setUser] = useState({ username: "", password: "" });
   const { login } = useAuth();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [error, setError] = useState({
+    usernameError: "",
+    passwordError: "",
+    detailError: "",
+  });
   function handleChange(e) {
     setUser({ ...user, [e.target.name]: e.target.value });
   }
 
   async function handleSubmit(e) {
     e.preventDefault();
+    setIsSubmitting(true);
+    setError({
+      usernameError: "",
+      passwordError: "",
+      detailError: "",
+    });
     try {
       const response = await api.post("auth/login/", user);
       const { access, refresh } = response.data;
       login(access, refresh);
+      toast.success("Logged in successfully");
       navigate("/dashboard");
-      console.log("Succes: ", response.data);
     } catch (error) {
       if (error.response) {
+        const backendErrors = error.response.data;
         console.log("Error data: ", error.response.data);
+        setError({
+          usernameError: backendErrors.username?.[0] || "",
+          passwordError: backendErrors.password?.[0] || "",
+          detailError: backendErrors.detail || "",
+        });
       } else {
         console.log("Error message: ", error.message);
       }
+    } finally {
+      setIsSubmitting(false);
     }
   }
 
@@ -68,6 +91,9 @@ export function LoginPage() {
             value={user.username}
             onChange={handleChange}
           />
+          {error.usernameError && (
+            <p className="backend-error">{error.usernameError}</p>
+          )}
           <label>Password</label>
           <input
             type="password"
@@ -75,7 +101,29 @@ export function LoginPage() {
             value={user.password}
             onChange={handleChange}
           />
-          <button type="submit">Start Writing</button>
+          {error.passwordError && (
+            <p className="backend-error">{error.passwordError}</p>
+          )}
+          {error.detailError && (
+            <p className="backend-error">{error.detailError}</p>
+          )}
+          <button type="submit" disabled={isSubmitting}>
+            {isSubmitting ? (
+              <motion.span
+                className="submit-loader"
+                animate={{ rotate: 360 }}
+                transition={{
+                  repeat: Infinity,
+                  duration: 1,
+                  ease: "linear",
+                }}
+              >
+                <Loader2 size={20} />
+              </motion.span>
+            ) : (
+              "Login"
+            )}
+          </button>
         </form>
       </div>
       <div className="note-div">
